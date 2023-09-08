7ead60cbf63fa0f35f0a7431a62f98328565a09e
SaifeddineBarkia <saifedinebarkia@gmail.com>
2023-09-09 00:09:46 +0200

    Section2 - Finished

--- app_section1/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    DB_HOST = os.getenv('DB_HOST', 'mydatabase.com')
-    DB_USERNAME= os.getenv('DB_USERNAME')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
--- app_section1/exceptions.py
+++
@@ -1,3 +0,0 @@
-class UserNotFound (Exception):
-    def __init__(self, user_id):
-        self.user_id = user_id
